59ba4a9235841940897929c8e6e96e2e119d1769
Yejeong Ham <dev@thelightway.kr>
2026-01-22 15:15:05 +0900

    added: patch.bat

--- patch.bat
+++ patch.bat
@@ -1,5 +1,2 @@
-git log --oneline 9df19dedaf3c4f31382573c379cd0283599980ab..spring/main -- framework-docs
-git fetch spring
-md tmp\patches
-git format-patch 9df19dedaf3c4f31382573c379cd0283599980ab..spring/main  -o tmp/patches -- framework-docs
-echo " git am tmp/patches/*.patch"
+git fetch spring main
+git subtree pull --prefix=origin spring main
